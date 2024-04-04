26105515b3b5f08beb88bb25ffde8c8d816d8460
hashmi633 <106319459+hashmi633@users.noreply.github.com>
2024-04-04 20:20:33 +0500

    todo add and get todos

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-1. to run server `poetry run uvicorn app.main:todo_server --reload`
+1. `poetry add fastapi uvicorn`
+1. to run server `poetry run uvicorn app.main:todo_app --reload --port=8000`
+2. `poetry add sqlmodel`
+3. `poetry add "psycopg[binary]"` 
+
+# Code
+1. Creation of object of the application
+2. creation of base routes
+2. database schema
+3. connection to the database
+    * connection string using database url
+4. creation of table when application startup (using contextlib)
+5. insert data in database
+6. 
--- app/main.py
+++ app/main.py
@@ -1,19 +1,80 @@
-from fastapi import FastAPI
+from typing import Optional, Annotated
+from fastapi import FastAPI, HTTPException, Depends, Query
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from contextlib import asynccontextmanager
 from app import settings
-
-# Connection to the database
-# Creation of table
-# Data save in table
+from pydantic import BaseModel
 
 
-todo_server: FastAPI = FastAPI()
+# database schema
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    todo: str = Field(index=True)
+    is_complete: bool = Field(default=False)
 
 
-@todo_server.get("/")
+# database connection
+connection_string: str = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
+engine = create_engine(
+    connection_string, connect_args={"sslmode": "require"}, pool_recycle=500
+)
+
+
+# creation of table
+def create_db_table():
+    print("creating table...")
+    SQLModel.metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def life_span(server: FastAPI):
+    print("server startup")
+    create_db_table()
+    yield
+
+
+# creation of application
+todo_app: FastAPI = FastAPI(
+    lifespan=life_span,
+    title="todo app",
+    version="0.0.1",
+    servers=[{"url": "http:/0.0.0.0:8000", "description": "Development Server"}],
+)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+# class TodoResponse(BaseModel):
+#     message: str
+#     todo: Todo
+
+
+@todo_app.post("/todo/", response_model=Todo)
+def get_todos(todo: Todo, session: Annotated[Session, Depends(get_session)]):
+    # if todo:
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
+    # return TodoResponse(message="todo added successfully", todo=todo)
+
+
+@todo_app.get("/todo/", response_model=list[Todo])
+def raed_todos(session: Annotated[Session, Depends(get_session)]):
+    todos = session.exec(select(Todo)).all()
+    return todos
+
+
+@todo_app.get("/")
 def hello():
-    return {"hello": "world"}
+    return {"Hello": "World"}
 
 
-@todo_server.get("/db")
-def db_var():
-    return {"DB": settings.DATABASE_URL}
+@todo_app.get("/db")
+def db_url():
+    return {"DB": settings.DATABASE_URL, "connection_string": connection_string}
